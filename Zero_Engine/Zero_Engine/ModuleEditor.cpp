9119bac7fdf5f885ff6a6c6144bcd171259f3917
Ian <itrueba0809@gmail.com>
2016-09-20 17:27:05 +0200

    First Commit

---
+++ Zero_Engine/Zero_Engine/ModuleEditor.cpp
@@ -0,0 +1,42 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleEditor.h"
+#include "Imgui\imgui.h"
+
+ModuleEditor::ModuleEditor(Application* app, bool start_enabled) : Module(app, start_enabled)
+{}
+
+ModuleEditor::~ModuleEditor()
+{}
+
+bool ModuleEditor::Start()
+{
+	return true;
+}
+
+bool ModuleEditor::CleanUp()
+{
+	return true;
+}
+
+update_status ModuleEditor::Update(float dt)
+{
+	//Create the menu bar
+	if (ImGui::BeginMainMenuBar())
+	{
+		if (ImGui::BeginMenu("File"))
+		{
+			ImGui::EndMenu();
+		}
+
+		if (ImGui::MenuItem("Quit", NULL))
+		{
+			return UPDATE_STOP;
+		}
+
+		ImGui::EndMainMenuBar();
+	}
+	ImGui::ShowTestWindow();
+
+	return UPDATE_CONTINUE;
+}
---
+++ Zero_Engine/Zero_Engine/ModuleEditor.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+
+class ModuleEditor : public Module
+{
+public:
+	ModuleEditor(Application* app, bool start_enabled = true);
+	~ModuleEditor();
+
+	bool Start();
+	update_status Update(float dt);
+	bool CleanUp();
+
+public:
+
+};
