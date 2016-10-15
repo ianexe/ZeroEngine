d3eeb7d2ae24da1d4d8b62a058125c2fa094d406
Ian <itrueba0809@gmail.com>
2016-10-15 02:59:09 +0200

    Simplifying Texture

--- Zero_Engine/Zero_Engine/Texture.h
+++ Zero_Engine/Zero_Engine/Texture.h
@@ -1,20 +1,8 @@
 #ifndef __TEXTURE_H__
 #define __TEXTURE_H__
 
-class Texture
+struct Texture
 {
-public:
-	Texture()
-	{}
-	~Texture()
-	{
-	}
-	void SetID(uint _id)
-	{
-		id = _id;
-	}
-
-public:
 	uint id = 0;
 	int width = 0;
 	int height = 0;
